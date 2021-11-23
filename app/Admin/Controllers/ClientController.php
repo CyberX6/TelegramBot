9190cf6955e735936abc8bbcc7b5ff517ee6017a
Lasha Palelashvili <falelashvili@gmail.com>
2021-11-23 06:43:51 +0400

    admin

---
+++ app/Admin/Controllers/ClientController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Client;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class ClientController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Client';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Client());
+
+        $grid->column('id', __('Id'))->sortable();
+        $grid->column('user_id', __('User id'));
+        $grid->column('name', __('Name'));
+        $grid->column('age', __('Age'));
+        $grid->column('status', __('Status'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Client::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('user_id', __('User id'));
+        $show->field('name', __('Name'));
+        $show->field('age', __('Age'));
+        $show->field('status', __('Status'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Client());
+
+        $form->number('user_id', __('User id'));
+        $form->text('name', __('Name'));
+        $form->number('age', __('Age'));
+        $form->number('status', __('Status'));
+
+        return $form;
+    }
+}
